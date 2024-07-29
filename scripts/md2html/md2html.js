39d365fb1a0881e12ec5c9e2696cf7f57bcf0ed2
Ralf Handl <ralf.handl@sap.com>
2024-07-29 11:13:44 +0200

    IANA registries now in Specref

--- scripts/md2html/md2html.js
+++ scripts/md2html/md2html.js
@@ -108,16 +108,6 @@ function preface(title,options) {
                 date: "2016-11-18",
                 authors: [ "John MacFarlane" ]
             },
-            "IANA-HTTP-AUTHSCHEMES": {
-                href: "https://www.iana.org/assignments/http-authschemes/",
-                title: "Hypertext Transfer Protocol (HTTP) Authentication Scheme Registry",
-                publisher: "IANA"
-            },
-            "IANA-HTTP-STATUS-CODES": {
-                href: "https://www.iana.org/assignments/http-status-codes/",
-                title: "Hypertext Transfer Protocol (HTTP) Status Code Registry",
-                publisher: "IANA"
-            },
             "JSON-Schema-Validation-04": {
                 authors: [ "Kris Zyp", "Francis Galiegue", "Gary Court" ],
                 href: "https://datatracker.ietf.org/doc/html/draft-fge-json-schema-validation-00",
