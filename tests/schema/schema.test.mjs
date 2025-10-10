5094265aaa89283734c9271b540c8523f81f6080
Ralf Handl <ralf.handl@gmail.com>
2025-10-10 23:20:35 +0200

    adjust schemas, test script, and test data

--- tests/schema/schema.test.mjs
+++ tests/schema/schema.test.mjs
@@ -13,12 +13,12 @@ await registerOasSchema();
 await registerSchema("./src/schemas/validation/schema.yaml");
 const fixtures = './tests/schema';
 
-describe("v3.2", () => {
+describe("v4.0", () => {
   test("schema.yaml schema test", async () => {
     // Files in the pass/fail folders get run against schema-base.yaml.
     // This instance is instead run against schema.yaml.
     const oad = {
-      openapi: "3.2.0",
+      openapi: "4.0.0",
       info: {
         title: "API",
         version: "1.0.0"
